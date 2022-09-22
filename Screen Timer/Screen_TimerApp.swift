a7546c2a293ab8a6ade7b874e534f716b7df0366
Cierra Morris <4cibear@gmail.com>
2022-09-22 16:36:53 -0600

    Initial Commit

---
+++ Screen Timer/Screen_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_TimerApp.swift
+//  Screen Timer
+//
+//  Created by Cierra Morris on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Screen_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
